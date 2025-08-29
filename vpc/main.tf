e2dbaaf093d4ae1afbb32838377f4d546a97c2f8
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-29 14:57:03 +0530

    jenkins-vpc-robo

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,20 +1,21 @@
 module "roboshop" {
-    #source = ".../terraform_aws_vpc"
-    source = "git::https://github.com/chinnulakku/terraform-aws-vpc.git?ref=main"
-    project_name = var.project_name
-    environment = var.environment
-    common_tags = var.common_tags
-    vpc_tags = var.vpc_tags
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
 
-    # public subnet
-    public_subnets_cidr = var.public_subnets_cidr
-    
-    #private subnet
-    private_subnets_cidr = var.private_subnets_cidr
-    
-    #database subnet
-    database_subnets_cidr = var.database_subnets_cidr
+  # public subnet
+  public_subnets_cidr = var.public_subnets_cidr
 
-    #peering
-    is_peering_required = var.is_peering_required
+  # private subnet
+  private_subnets_cidr = var.private_subnets_cidr
+
+  # database subnet
+  database_subnets_cidr = var.database_subnets_cidr
+
+  #peering
+  is_peering_required = var.is_peering_required
+
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,12 +1,12 @@
-variable "vpc_cidr"{
-    default = "10.0.0.0/16."
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 variable "common_tags" {
-    default = {
-        Project = "roboshop"
-        Environment = "dev"
-        Terraform = "true"
-    }
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terraform = "true"
+  }
 }
 
 variable "vpc_tags" {
@@ -14,25 +14,25 @@ variable "vpc_tags" {
 }
 
 variable "project_name" {
-    default = "roboshop"
+  default = "roboshop"
 }
 
 variable "environment" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "public_subnets_cidr" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnets_cidr" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "is_peering_required" {
-    default = true
+  default = true
 }
